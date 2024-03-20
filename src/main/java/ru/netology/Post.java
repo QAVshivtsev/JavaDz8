2d286d8f3c82e1c75fd0ac39800a3820bfce59d5
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-03-20 22:27:44 +0300

    first commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import ru.netology.FormDate;
+
+public class Post {
+    public String name; // "Иван"
+    public String patronymic; //"Иванович"
+    public String surname; //"Иванов"
+    public String passport; //"4444 № 44444444"
+    public String phone; //"+7 (999)-999-99-99"
+    public boolean subscription; //true
+    public FormDate birthday;
+}
